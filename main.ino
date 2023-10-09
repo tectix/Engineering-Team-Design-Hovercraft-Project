cf05e7146b0278dd290ef7114a18904fdb1ddf44
Alinoureddine1 <ali-nd2001@outlook.com>
2023-10-09 02:07:15 -0400

    Added a servo instance controlled using the yaw angle

--- main.ino
+++ main.ino
@@ -1,13 +1,16 @@
 #include <Wire.h>
 #include <MPU6050.h>
-
+#include <Servo.h>
 MPU6050 mpu;
 
+
+Servo sg90;
+int servo_pin = 6;
 // Timers
 unsigned long timer = 0;
 unsigned long printTimer = 0;
 float timeStep = 0.01; // Read data every 10 milliseconds
-unsigned long printInterval = 1000; // Print data every 1000 milliseconds (1 second)
+unsigned long printInterval = 500; // Print data every 1000 milliseconds (1 second)
 
 // Pitch, Roll, and Yaw values
 float pitch = 0;
@@ -15,13 +18,40 @@ float roll = 0;
 float yaw = 0;
 
 
-//Calibrating accels based on trials
-float xAccCalib = -0.06;
+//Calibrating accelerations based on trials
+float xAccCalib = -0.01;
 float yAccCalib =0.0;
 float zAccCalib = -0.06;
+float pitchCalib = -0.06;
+float rollCalib = -0.06;
+float yawCalib = -0.06;
+
+//LSB sensitivity constants for full scale range
+const float MPU6050_SENS_FOR_RANGE_2 = 16384.0;
+const float MPU6050_SENS_FOR_RANGE_4 = 8192.0;
+const float MPU6050_SENS_FOR_RANGE_8 = 4096.0;
+const float MPU6050_SENS_FOR_RANGE_16 = 2048.0;
+
+float lsbSensitivity = MPU6050_SENS_FOR_RANGE_2;
+
+
+
+// Initializing the mpu function, choose the appropriate scale/ range. the options are
+    // MPU6050_SCALE_2000DPS       - 250 deg/sec => sensitivity 16.4.0
+    // MPU6050_SCALE_1000DPS       - 1000 deg/sec => sensitivity 328.0
+    // MPU6050_SCALE_500DPS        - 500 deg/sec => sensitivity 655.0
+    // MPU6050_SCALE_250DPS        - 250 deg/sec => sensitivity 131.0
+
+// Change the LSB sensitivity constants when choosing a range
+    // MPU6050_RANGE_16G             = 16g = MPU6050_SENS_FOR_RANGE_16
+    // MPU6050_RANGE_8G              = 8g  = MPU6050_SENS_FOR_RANGE_8
+    // MPU6050_RANGE_4G              = 4g  = MPU6050_SENS_FOR_RANGE_4
+    // MPU6050_RANGE_2G              = 2g  = MPU6050_SENS_FOR_RANGE_2
+
 
 void setup() 
 {
+  sg90.attach(servo_pin);
   Serial.begin(115200);
 
   // Initialize MPU6050
@@ -32,9 +62,11 @@ void setup()
   }
   
   // Calibrate gyroscope. The calibration must be at rest.
+  // If you don't want calibrate, comment this line.
   mpu.calibrateGyro();
 
   // Set threshold sensitivity. Default 3.
+  // If you don't want to use threshold, comment this line or set 0.
   mpu.setThreshold(3);
 }
 
@@ -48,16 +80,17 @@ void loop()
   // Read normalized values from accelerometer
   Vector normAccel = mpu.readRawAccel();
 
-  // Calculate Pitch, Roll, and Yaw
+  // Calculate Pitch, Roll, and Yaw deg/s * s = deg
   pitch = pitch + normGyro.YAxis * timeStep;
   roll = roll + normGyro.XAxis * timeStep;
   yaw = yaw + normGyro.ZAxis * timeStep;
-
-  // Calculation of linear acceleration/forces 
   
-  float linearAccelX = normAccel.XAxis /16384.0 +xAccCalib;
-  float linearAccelY = normAccel.YAxis /16384.0 + yAccCalib;
-  float linearAccelZ = normAccel.ZAxis /16384.0 +zAccCalib; 
+  int servoMotion = constrain(yaw + 90, 0, 180);
+  sg90.write(servoMotion);
+  // Calculate linear accelerations
+  float linearAccelX = normAccel.XAxis /lsbSensitivity +xAccCalib;
+  float linearAccelY = normAccel.YAxis /lsbSensitivity + yAccCalib;
+  float linearAccelZ = normAccel.ZAxis /lsbSensitivity +zAccCalib; 
 
   // Check if it's time to print the values (once every second)
   if (millis() - printTimer >= printInterval)
