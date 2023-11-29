2d3c8b7c5a7ba3329d36f79fe6702d771a78bfe3
Ali Noureddine <ali-nd2001@outlook.com>
2023-11-28 22:04:07 -0500

    more adjustments

--- Hovercraft/Hovercraft/Hovercraft.ino
+++ Hovercraft/Hovercraft/Hovercraft.ino
@@ -11,7 +11,7 @@
 #define MAX_DISTANCE 400
 #define SERVO_PIN 6
 #define FORWARD_TIME 6000 // Time in milliseconds to move forward 
-#define TURN_TIME 2000 // Estimated time to complete a U-turn 
+#define TURN_TIME 3000 // Estimated time to complete a U-turn 
 #define OBSTACLE_DISTANCE 15 // Distance in cm to detect an obstacle
 
 
@@ -37,23 +37,29 @@ float distanceRight = 0;
 
 
 //Competition states
-enum State {
-    WAITING,
-    TURNING_LEFT,
-    MEASURING_LEFT,
-    TURNING_RIGHT,
-    MEASURING_RIGHT,
-    DECIDING_DIRECTION
-};
-State currentState = WAITING;
+// enum State {
+//     WAITING,
+//     TURNING_LEFT,
+//     MEASURING_LEFT,
+//     TURNING_RIGHT,
+//     MEASURING_RIGHT,
+//     DECIDING_DIRECTION,
+//     DONE_DECIDING
+// };
+// State currentState = WAITING;
 
 //Demo states
-// enum State {
-//     MOVING_FORWARD,
-//     TURNING,
-//     COMPLETED
-// };
-// State currentState = MOVING_FORWARD;
+enum State {
+  WAITING,
+    MOVING_FORWARD,
+    
+    TURNING,
+    TURNING2,
+    COMPLETED,
+    STOPPING,
+    MOVING_BACK
+};
+State currentState = MOVING_FORWARD;
 
 
 
@@ -64,9 +70,10 @@ void setup() {
   serv.attach(SERVO_PIN);
   initIMU();
   initLiftFan();
-  startLiftFan();
+  
   initThrustFan();
   startThrustFan();
+  startLiftFan();
   servoMiddle();
 }
  
@@ -82,61 +89,72 @@ void loop() {
     Serial.println(" cm");
 
 
-     if (distance < OBSTACLE_DISTANCE && currentState == WAITING) {
-        // Stop hovercraft
-        stopThrustFan();
-        stopLiftFan();
+    //  if (distance < OBSTACLE_DISTANCE && currentState == WAITING) {
+    //     // Stop hovercraft
+    //     stopThrustFan();
+    //     stopLiftFan();
 
-          // Turn left
-        currentState = TURNING_LEFT;
-        servoLeft();
-        previousMillis = currentMillis;
-    }
+    //       // Turn left
+    //      currentState = TURNING_LEFT;
+        
+    //     servoLeft();
+    //     previousMillis = currentMillis;
+    // }
 
-    switch (currentState) {
-        case TURNING_LEFT:
-            if (currentMillis - previousMillis >= servoInterval) {
-                currentState = MEASURING_LEFT;
-                previousMillis = currentMillis;
-            }
-            break;
+    // switch (currentState) {
+    //     case TURNING_LEFT:
+    //         if (currentMillis - previousMillis >= servoInterval) {
+    //             currentState = MEASURING_LEFT;
+    //             previousMillis = currentMillis;
+    //         }
+    //         break;
 
-        case MEASURING_LEFT:
-            if (currentMillis - previousMillis >= servoInterval) {
-                distanceLeft = calculateDistance();
-                currentState = TURNING_RIGHT;
-                servoRight();
-                previousMillis = currentMillis;
-            }
-            break;
+    //     case MEASURING_LEFT:
+    //         if (currentMillis - previousMillis >= servoInterval) {
+    //             distanceLeft = calculateDistance();
+    //             currentState = TURNING_RIGHT;
+    //             servoRight();
+    //             previousMillis = currentMillis;
+    //         }
+    //         break;
 
-        case TURNING_RIGHT:
-            if (currentMillis - previousMillis >= servoInterval) {
-                currentState = MEASURING_RIGHT;
-                previousMillis = currentMillis;
-            }
-            break;
+    //     case TURNING_RIGHT:
+    //         if (currentMillis - previousMillis >= servoInterval) {
+    //             currentState = MEASURING_RIGHT;
+    //             previousMillis = currentMillis;
+    //         }
+    //         break;
 
-        case MEASURING_RIGHT:
-            if (currentMillis - previousMillis >= servoInterval) {
-                distanceRight = calculateDistance();
-                currentState = DECIDING_DIRECTION;
-            }
-            break;
+    //     case MEASURING_RIGHT:
+    //         if (currentMillis - previousMillis >= servoInterval) {
+    //             distanceRight = calculateDistance();
+    //             currentState = DECIDING_DIRECTION;
+    //         }
+    //         break;
 
-        case DECIDING_DIRECTION:
-            if (distanceLeft > distanceRight) {
-                servoLeft();
-            } else {
-                servoRight();
-            }
-            startThrustFan(); // Restart hovercraft movement
-            currentState = WAITING;
-            break;
+    //     case DECIDING_DIRECTION:
+    //         if (distanceLeft > distanceRight) {
+    //             servoLeft();
+    //         } else {
+    //             servoRight();
+    //         }
+    //          startLiftFan();
+    //         startThrustFan(); // Restart hovercraft movement
+    //         previousMillis = currentMillis;
+    //         currentState = DONE_DECIDING;
+    //         break;
 
-        default:
-            break;
-    }
+    //       case DONE_DECIDING:
+    //       if (currentMillis - previousMillis >= servoInterval + 500) {
+    //             servoMiddle();
+    //             currentState = WAITING;
+    //             previousMillis = currentMillis;
+    //         }
+    //         break;
+
+    //     default:
+    //         break;
+    // }
 
 
 
@@ -144,43 +162,55 @@ void loop() {
 
 
     //SWitch logic for the demo
-    //  switch (currentState) {
-    //     case MOVING_FORWARD:
-    //         if (currentTime - startTime >= FORWARD_TIME) {
-    //             stopThrustFan();
-    //             currentState = TURNING;
-    //             servoRight(); // Initiate U-turn to the right
-    //             startThrustFan();
-    //             startTime = currentTime;
-    //         }
-    //         break;
+     switch (currentState) {
+        case MOVING_FORWARD:
+            if (currentMillis - previousMillis >= FORWARD_TIME) {
+                //stopLiftFan();
+               // stopThrustFan();
+                currentState = TURNING;
+                servoLeft(); // Initiate U-turn to the right
+        
+                previousMillis = currentMillis;
+            }
+            break;
 
-    //     case TURNING:
-    //         if (currentTime - startTime > TURN_TIME) {
-    //             stopThrustFan();
-    //             currentState = MOVING_BACK;
-    //             servoMiddle(); // Reset servo to move straight back
-    //             startThrustFan();
-    //             startTime = currentTime;
-    //         }
-    //         break;
+        case TURNING:
+            if (currentMillis - previousMillis >= TURN_TIME -1500) {
+               // startLiftFan();
+               // startThrustFan();
+                currentState = TURNING2;
+                servoMiddle(); // Reset servo to move straight back
+               // startThrustFan();
+                previousMillis = currentMillis;
+            }
+            break;
 
-    //     case MOVING_BACK:
-    //         if (distance < OBSTACLE_DISTANCE) {
-    //             stopThrustFan();
-    //             currentState = STOPPING;
-    //         }
-    //         break;
+            case TURNING2:
+            if (currentMillis - previousMillis >= TURN_TIME -2500) {
+                currentState = MOVING_BACK;
+                servoMiddle(); // Reset servo to move straight back
+               // startThrustFan();
+                previousMillis = currentMillis;
+            }
+            break;
 
-    //     case STOPPING:
-    //         // Hovercraft has stopped due to an obstacle
-    //         currentState = COMPLETED;
-    //         break;
+        case MOVING_BACK:
+            if (distance < OBSTACLE_DISTANCE) {
+                stopThrustFan();
+                stopLiftFan();
+                currentState = STOPPING;
+            }
+            break;
 
-    //     case COMPLETED:
-    //
-    //         break;
-    // }
+        case STOPPING:
+            // Hovercraft has stopped due to an obstacle
+            currentState = COMPLETED;
+            break;
+
+        case COMPLETED:
+    
+            break;
+    }
 
 
 
@@ -199,11 +229,15 @@ serv.write(0);
 }
 
 void servoRight(){
-serv.write(185);
+serv.write(183);
+}
+
+void servoCustom(int angle){
+serv.write(angle);
 }
 
 void servoMiddle(){
-  serv.write(90);
+  serv.write(89);
 }
 
 
