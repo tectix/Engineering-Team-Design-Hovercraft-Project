f7bac399e94dbb3e3a946746b5274d0808f76f94
Ali Noureddine <ali-nd2001@outlook.com>
2023-11-30 16:59:47 -0500

    Added imu logic

--- Hovercraft/Hovercraft/Hovercraft.ino
+++ Hovercraft/Hovercraft/Hovercraft.ino
@@ -10,13 +10,15 @@
 #define ECHO_PIN     3
 #define MAX_DISTANCE 400
 #define SERVO_PIN 6
-#define FORWARD_TIME 6000 // Time in milliseconds to move forward 
-#define TURN_TIME 3000 // Estimated time to complete a U-turn 
-#define OBSTACLE_DISTANCE 15 // Distance in cm to detect an obstacle
+#define FORWARD_TIME 4000 // Time in milliseconds to move forward 
+#define TURN_TIME 500 // Estimated time to complete a U-turn 
+#define OBSTACLE_DISTANCE 30 // Distance in cm to detect an obstacle
 
 
 float currentYaw  = 0;
 float targetYaw = 0;
+bool isTurning = false;
+const float YAW_TOLERANCE = 5;
 unsigned long timer = 0;
 float timeStep = 0.1; // Read data every 10 milliseconds
 
@@ -36,33 +38,17 @@ float distanceRight = 0;
 
 
 
-//Competition states
-// enum State {
-//     WAITING,
-//     TURNING_LEFT,
-//     MEASURING_LEFT,
-//     TURNING_RIGHT,
-//     MEASURING_RIGHT,
-//     DECIDING_DIRECTION,
-//     DONE_DECIDING
-// };
-// State currentState = WAITING;
-
-//Demo states
 enum State {
-  WAITING,
-    MOVING_FORWARD,
-    
-    TURNING,
-    TURNING2,
-    COMPLETED,
-    STOPPING,
-    MOVING_BACK
+    WAITING,
+    TURNING_LEFT,
+    MEASURING_LEFT,
+    TURNING_RIGHT,
+    MEASURING_RIGHT,
+    DECIDING_DIRECTION,
+    DONE_DECIDING,
+    ADJUST_IMU
 };
-State currentState = MOVING_FORWARD;
-
-
-
+State currentState = WAITING;
 
 
 void setup() {
@@ -80,137 +66,94 @@ void setup() {
 void loop() {
 
     unsigned long currentMillis = millis();
-    Vector normGyro = mpu.readNormalizeGyro();
-    currentYaw  = currentYaw  + normGyro.ZAxis * timeStep;
-    Serial.print(" Yaw = "); Serial.println(currentYaw );
+   // Vector normGyro = mpu.readNormalizeGyro();
+  //  currentYaw  = currentYaw  + normGyro.ZAxis * timeStep;
+   // Serial.print(" Yaw = "); Serial.println(currentYaw );
+    updateYaw();
     distance = calculateDistance();
     Serial.print("Distance = ");
     Serial.print(distance);
     Serial.println(" cm");
 
 
-    //  if (distance < OBSTACLE_DISTANCE && currentState == WAITING) {
-    //     // Stop hovercraft
-    //     stopThrustFan();
-    //     stopLiftFan();
+     if (distance < OBSTACLE_DISTANCE && currentState == WAITING  &&!isTurning) {
+        // Stop hovercraft
+        stopThrustFan();
+        stopLiftFan();
 
-    //       // Turn left
-    //      currentState = TURNING_LEFT;
+        //   // Turn left
+         currentState = TURNING_LEFT;
         
-    //     servoLeft();
-    //     previousMillis = currentMillis;
-    // }
+        servoLeft();
+        previousMillis = currentMillis;
+    }
 
-    // switch (currentState) {
-    //     case TURNING_LEFT:
-    //         if (currentMillis - previousMillis >= servoInterval) {
-    //             currentState = MEASURING_LEFT;
-    //             previousMillis = currentMillis;
-    //         }
-    //         break;
+    switch (currentState) {
+        case TURNING_LEFT:
+            if (currentMillis - previousMillis >= servoInterval) {
+                currentState = MEASURING_LEFT;
+                previousMillis = currentMillis;
+            }
+            break;
 
-    //     case MEASURING_LEFT:
-    //         if (currentMillis - previousMillis >= servoInterval) {
-    //             distanceLeft = calculateDistance();
-    //             currentState = TURNING_RIGHT;
-    //             servoRight();
-    //             previousMillis = currentMillis;
-    //         }
-    //         break;
+        case MEASURING_LEFT:
+            if (currentMillis - previousMillis >= servoInterval) {
+                distanceLeft = calculateDistance();
+                currentState = TURNING_RIGHT;
+                servoRight();
+                previousMillis = currentMillis;
+            }
+            break;
 
-    //     case TURNING_RIGHT:
-    //         if (currentMillis - previousMillis >= servoInterval) {
-    //             currentState = MEASURING_RIGHT;
-    //             previousMillis = currentMillis;
-    //         }
-    //         break;
+        case TURNING_RIGHT:
+            if (currentMillis - previousMillis >= servoInterval) {
+                currentState = MEASURING_RIGHT;
+                previousMillis = currentMillis;
+            }
+            break;
 
-    //     case MEASURING_RIGHT:
-    //         if (currentMillis - previousMillis >= servoInterval) {
-    //             distanceRight = calculateDistance();
-    //             currentState = DECIDING_DIRECTION;
-    //         }
-    //         break;
+        case MEASURING_RIGHT:
+            if (currentMillis - previousMillis >= servoInterval) {
+                distanceRight = calculateDistance();
+                currentState = DECIDING_DIRECTION;
+            }
+            break;
 
-    //     case DECIDING_DIRECTION:
-    //         if (distanceLeft > distanceRight) {
-    //             servoLeft();
-    //         } else {
-    //             servoRight();
-    //         }
-    //          startLiftFan();
-    //         startThrustFan(); // Restart hovercraft movement
-    //         previousMillis = currentMillis;
-    //         currentState = DONE_DECIDING;
-    //         break;
+        case DECIDING_DIRECTION:
+             
+            if (distanceLeft > distanceRight) {
+                servoCustom(45);
+            } else {
+                servoCustom(135);
+            }
+            startLiftFan();
+            startThrustFan();
+            
+            // Restart hovercraft movement
+            previousMillis = currentMillis;
+            currentState = DONE_DECIDING;
+            break;
 
-    //       case DONE_DECIDING:
-    //       if (currentMillis - previousMillis >= servoInterval + 500) {
-    //             servoMiddle();
-    //             currentState = WAITING;
-    //             previousMillis = currentMillis;
-    //         }
-    //         break;
-
-    //     default:
-    //         break;
-    // }
-
-
-
-
-
-
-    //SWitch logic for the demo
-     switch (currentState) {
-        case MOVING_FORWARD:
-            if (currentMillis - previousMillis >= FORWARD_TIME) {
-                //stopLiftFan();
-               // stopThrustFan();
-                currentState = TURNING;
-                servoLeft(); // Initiate U-turn to the right
+          case DONE_DECIDING:
+          if (currentMillis - previousMillis >= servoInterval+ 1500) {
+                servoMiddle();
+                currentState = WAITING;
+                previousMillis = currentMillis;
+                isTurning = false;
+            }
+            break;
         
-                previousMillis = currentMillis;
-            }
-            break;
 
-        case TURNING:
-            if (currentMillis - previousMillis >= TURN_TIME -1500) {
-               // startLiftFan();
-               // startThrustFan();
-                currentState = TURNING2;
-                servoMiddle(); // Reset servo to move straight back
-               // startThrustFan();
-                previousMillis = currentMillis;
-            }
-            break;
+        case ADJUST_IMU:
+         break;
 
-            case TURNING2:
-            if (currentMillis - previousMillis >= TURN_TIME -2500) {
-                currentState = MOVING_BACK;
-                servoMiddle(); // Reset servo to move straight back
-               // startThrustFan();
-                previousMillis = currentMillis;
-            }
-            break;
-
-        case MOVING_BACK:
-            if (distance < OBSTACLE_DISTANCE) {
-                stopThrustFan();
-                stopLiftFan();
-                currentState = STOPPING;
-            }
-            break;
-
-        case STOPPING:
-            // Hovercraft has stopped due to an obstacle
-            currentState = COMPLETED;
-            break;
-
-        case COMPLETED:
-    
+        default:
             break;
     }
+
+
+
+
 
 
 
@@ -241,36 +184,71 @@ void servoMiddle(){
 }
 
 
-void resetGyro() {
-    currentYaw  = 0;
+
+void updateYaw() {
+    Vector normGyro = mpu.readNormalizeGyro();
+    currentYaw += normGyro.ZAxis * timeStep;
+    currentYaw = normalizeYaw(currentYaw); // Correctly normalize the yaw value
+}
+
+float calculateTargetYaw(float turnAngle) {
+    float newYaw = currentYaw + turnAngle;
+    return normalizeYaw(newYaw); // Adjust for wrapping and return
+}
+
+bool hasReachedTargetYaw() {
+    return abs(normalizeYaw(currentYaw) - targetYaw) < YAW_TOLERANCE;
+}
+
+float normalizeYaw(float yaw) {
+    while (yaw < 0) yaw += 360;
+    while (yaw >= 360) yaw -= 360;
+    return yaw;
+}
+
+void stopMovement() {
+    stopThrustFan();
+    stopLiftFan();
+    servoMiddle();
+}
+
+void recalibrateGyro() {
     mpu.calibrateGyro();
+    currentYaw = 0; // Reset yaw after calibration
 }
 
 
+//  case TURNING_LEFT:
+//             if (!isTurning) {
+//                 targetYaw = calculateTargetYaw(90); // 90 degrees left turn
+//                 isTurning = true;
+//                 servoLeft(); // Start turning left
+//             } else if (hasReachedTargetYaw()) {
+//                 currentState = MEASURING_LEFT;
+//                 isTurning = false;
+//                 previousMillis = currentMillis;
+//                 servoMiddle(); // Stop turning
+//             }
+//             break;
 
-//IMU verification inside the decision tree
-  // switch (currentState) {
-  //       case TURNING_LEFT:
-  //           if (!isTurning) {
-  //               targetYaw = currentYaw - 90; // Adjust for 90 degrees turn
-  //               isTurning = true;
-  //           }
-  //           if (currentYaw <= targetYaw) {
-  //               currentState = MEASURING_LEFT;
-  //               isTurning = false;
-  //               previousMillis = currentMillis;
-  //           }
-  //           break;
+//         case TURNING_RIGHT:
+//             if (!isTurning) {
+//                 targetYaw = calculateTargetYaw(-90); // 90 degrees right turn
+//                 isTurning = true;
+//                 servoRight(); // Start turning right
+//             } else if (hasReachedTargetYaw()) {
+//                 currentState = MEASURING_RIGHT;
+//                 isTurning = false;
+//                 previousMillis = currentMillis;
+//                 servoMiddle(); // Stop turning
+//             }
+//             break;
 
-  //       case TURNING_RIGHT:
-  //           if (!isTurning) {
-  //               targetYaw = currentYaw + 90; // Adjust for 90 degrees turn
-  //               isTurning = true;
-  //           }
-  //           if (currentYaw >= targetYaw) {
-  //               currentState = MEASURING_RIGHT;
-  //               isTurning = false;
-  //               previousMillis = currentMillis;
-  //           }
-  //           break;
 
+
+//         case DECIDING_DIRECTION:
+//             // ... [Decision logic]
+//             startLiftFan();
+//             startThrustFan();
+//             currentState = DONE_DECIDING;
+//             break;
